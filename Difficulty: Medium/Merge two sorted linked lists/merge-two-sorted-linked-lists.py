0e9a0a3ec92646f0fd927d8331daa9511ccb3129
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-01-20 05:25:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
@@ -0,0 +1,89 @@
+#User function Template for python3
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+'''
+class Solution:
+    def sortedMerge(self,head1, head2):
+        arr = []
+        while head1 is not None:
+            arr.append(head1.data)
+            head1 = head1.next
+            
+        while head2 is not None:
+            arr.append(head2.data)
+            head2 = head2.next
+        arr.sort()
+        
+        dummy = Node(-1)
+        curr = dummy
+        
+        for value in arr:
+            curr.next = Node(value)
+            curr = curr.next
+        return dummy.next
+        
+        def printList(curr):
+            while curr is not None:
+                print(curr.data, end=" ")
+                curr = curr.next
+            print()
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(head):
+    temp = head
+    while temp:
+        print(temp.data, end=' ')
+        temp = temp.next
+    print()
+    print("~")
+
+
+def insert_sorted(head, data):
+    new_node = Node(data)
+    if not head or head.data >= data:
+        new_node.next = head
+        return new_node
+
+    current = head
+    while current.next and current.next.data < data:
+        current = current.next
+
+    new_node.next = current.next
+    current.next = new_node
+
+    return head
+
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        input1 = list(map(int, input().split()))
+        input2 = list(map(int, input().split()))
+
+        head1 = None
+        for item in input1:
+            head1 = insert_sorted(head1, item)
+
+        head2 = None
+        for item in input2:
+            head2 = insert_sorted(head2, item)
+
+        obj = Solution()
+        merged_head = obj.sortedMerge(head1, head2)
+        print_list(merged_head)
+
+# } Driver Code Ends
