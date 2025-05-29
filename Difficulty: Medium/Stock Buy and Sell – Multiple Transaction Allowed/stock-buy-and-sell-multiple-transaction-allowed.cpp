28b440dfba05d5546b7a2ff8cce681c57f74647e
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2025-05-29 06:39:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int n=prices.size();
+        int count=0;
+        for(int i=1;i<n;i++){
+            if(prices[i] > prices[i-1]){
+                count += prices[i] - prices[i-1];
+            }
+        }
+        return count;
+    }
+};
